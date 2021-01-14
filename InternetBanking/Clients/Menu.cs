279bf1567371715ffdf5fd8ef039008fa7952516
Chenyu Xiao <s3829221@student.rmit.edu.au>
2021-01-14 16:54:33 +1100

    Implement menu feature

--- InternetBanking/Clients/Menu.cs
+++ InternetBanking/Clients/Menu.cs
@@ -15,6 +15,8 @@ namespace Client
 
         private readonly CustomerManager _customerManager;
         private readonly LoginManager _loginManager;
+        private Customer _customer;
+        private Account _account;
 
 
         public Menu(string connectionString)
@@ -61,7 +63,7 @@ Enter an option: ");
             var loginID = Console.ReadLine();
             Console.WriteLine();
 
-            // Verify login ID
+            // Verify login ID / Correct login ID
             if (_loginManager.Logins.ContainsKey(loginID))
             {
                 var passwordHash = _loginManager.Logins[loginID].PasswordHash;
@@ -76,12 +78,47 @@ Enter an option: ");
                 // Verify password
                 var isCorrect = PBKDF2.Verify(passwordHash, pwd);
 
+                // Correct password / after login
                 if (isCorrect)
                 {
-                    Console.WriteLine($"Hello, Ms/Mr. {customer.Name}!");
-                    Console.WriteLine();
+                    _customer = customer;
 
+                    while (true)
+                    {
+                        Console.WriteLine($"Hello, Ms/Mr. {_customer.Name}!");
+                        Console.WriteLine();
+                        Console.Write(
+    @"  Customer Page
+=================
+1. Select account
+2. Log out
+
+Enter an option: ");
+
+                        var option = InputUtilities.EnterOption(1, 2);
+
+                        switch (option)
+                        {
+                            case 1:
+                                Console.Clear();
+                                SelectAccount();
+                                break;
+                            case 2:
+                                Console.Clear();
+
+                                // Remove the current operating customer
+                                _customer = null;
+
+                                Console.WriteLine("You have logged out successfully.");
+                                Console.WriteLine();
+                                return;
+                            default:
+                                throw new InvalidOperationException();
+                        } 
+                    }
                 }
+
+                // Incorrect password
                 else
                 {
                     Console.Clear();
@@ -90,12 +127,98 @@ Enter an option: ");
                     return;
                 }
             }
+
+            // Login ID does not exist
             else
             {
                 Console.Clear();
                 Console.WriteLine("Login ID does not exist, please try to log in again.");
                 Console.WriteLine();
                 return;
+            }
+        }
+
+
+        public void SelectAccount()
+        {
+            while (true)
+            {
+                Console.WriteLine("You own the following account(s): ");
+                foreach (var account in _customer.Accounts)
+                {
+                    Console.WriteLine(account.AccountNumber);
+                }
+
+                Console.WriteLine();
+                Console.Write("Select an account: ");
+
+                var accountNumber = Console.ReadLine();
+                Console.Clear();
+                
+                // Verify account number using LINQ
+                var result = from account in _customer.Accounts
+                             where account.AccountNumber.ToString().Equals(accountNumber)
+                             select account;
+
+                // Correct account number
+                if (result.Any())
+                {
+                    _account = result.First();
+                    SelectTransaction();
+                    return;
+                }
+
+                // Incorrect account number
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("This account number does not exist.");
+                    Console.WriteLine();
+                    continue;
+                }
+            }
+            
+        }
+
+
+        public void SelectTransaction()
+        {
+            while (true)
+            {
+                Console.Write(
+@"Select a service
+==========================
+1. Deposit
+2. Withdraw
+3. Transfer
+4. Return to customer page
+
+Enter an option: ");
+
+                var option = InputUtilities.EnterOption(1, 4);
+
+                switch (option)
+                {
+                    case 1:
+                        Console.Clear();
+
+                        break;
+                    case 2:
+                        Console.Clear();
+
+                        break;
+                    case 3:
+                        Console.Clear();
+
+                        break;
+                    case 4:
+                        Console.Clear();
+                        // Remove current operating account
+                        _account = null;
+                        return;
+                    default:
+                        throw new InvalidOperationException();
+                }
             }
         }
     }
